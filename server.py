d96ec8d26c418efeb2c6c27e623596e9041b811a
Nicolás Caytuiro <ncaytuiro@bantotal.com>
2024-06-14 10:03:13 -0500

    Modificación para trabajar con base de datos vectorial FAISS

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma, FAISS
 from langchain_openai.embeddings import AzureOpenAIEmbeddings
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage
@@ -37,7 +37,38 @@ async def home(request: Request):
 def health_check():
     return 'OK'
 
+# Con FAISS
 @app.post("/uploadFile")
+async def upload_file(file: UploadFile = File(...)):
+    global vector_store, chat_history
+    try:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            shutil.copyfileobj(file.file, temp_file)
+            document_path = temp_file.name
+
+        # Process the PDF
+        loader = PyPDFLoader(document_path)
+        documents = loader.load_and_split()
+        text_splitter = CharacterTextSplitter(chunk_size=1200, chunk_overlap=25)
+        docs = text_splitter.split_documents(documents)
+        embeddings = AzureOpenAIEmbeddings()
+        vector_store = FAISS.from_documents(docs, embeddings)
+        
+        # Verificar el contenido del vector store
+        print("Documentos cargados en el vector store:")
+        for doc in docs:
+            print(doc.page_content[:200])  # Imprime los primeros 200 caracteres de cada documento
+
+        return {"message": f"El archivo '{file.filename}' ha sido cargado y procesado correctamente."}
+    
+    except Exception as e:
+        # Capturar y mostrar el error detallado
+        error_message = f"Error procesando el archivo: {str(e)}"
+        print(error_message)
+        return JSONResponse(content={"message": error_message}, status_code=500)
+
+# Con ChromaDB
+""" @app.post("/uploadFile")
 async def upload_file(file: UploadFile = File(...)):
     global vector_store, chat_history
     try:
@@ -77,7 +108,7 @@ async def upload_file(file: UploadFile = File(...)):
         # Capturar y mostrar el error detallado
         error_message = f"Error procesando el archivo: {str(e)}"
         print(error_message)
-        return JSONResponse(content={"message": error_message}, status_code=500)
+        return JSONResponse(content={"message": error_message}, status_code=500) """
 
 """ @app.post("/uploadFile")
 async def upload_file(file: UploadFile = File(...)):
@@ -111,7 +142,85 @@ async def upload_file(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": "Error procesando la carga del archivo"}, status_code=500) """
 
+# Con FAISS
 @app.post("/send")
+async def send_message(request: Request):
+    global vector_store, chat_history
+
+    data = await request.json()  # Obtén el JSON del cuerpo de la solicitud
+    question = data.get('message')  # Accede al campo 'message'
+
+    print(question)
+
+    if vector_store is None:
+        return JSONResponse(content={"message": "No PDF file uploaded"}, status_code=400)
+
+    try:
+        # Definir el template y las variables de entrada
+        template = """
+        As a highly specialized assistant in helping Bantotal's analyst programmers understand the development of a requirement in Genexus, your task is to respond to questions in Spanish related to client requirements. 
+        These documents contain information about the requirements and the technical solution, including the logic that the program will execute, the database tables involved, and the programs related to the solution's development.
+
+        When answering questions, ensure that all information comes from the PDF document, previous chat history, or your prior knowledge of Bantotal and Genexus 8, 9, and 16. If a question cannot be answered, honestly state that you cannot answer it. 
+        Your responses should be detailed and focused on the question, providing all relevant details necessary for a complete answer without including information beyond the scope of the question.
+
+        Context: {context}
+
+        Question: {question}
+
+        Helpful Answer:
+        """
+
+        input_variables = ["context", "question"]
+        QA_CHAIN_PROMPT = PromptTemplate(template=template, input_variables=input_variables)
+
+        # Configuración del modelo LLM y embeddings
+        llm = AzureChatOpenAI(
+            deployment_name="gpt-35-turbo-16k", 
+            temperature=0
+        )
+
+        # Crear la cadena de conversación con recuperación utilizando un prompt personalizado
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=llm,
+            retriever=vector_store.as_retriever(search_kwargs={"k": 1}),
+            return_source_documents=True,
+            combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT},
+        )
+
+        # Generar el contexto solo con mensajes de usuario y respuestas del asistente
+        context_list = []
+        documents = vector_store.similarity_search(query=question, k=5)
+        for doc in documents:
+            context_list.append(doc.page_content)
+        context = " ".join(context_list)
+
+        # Pasar la cadena de texto como contexto en el diccionario query
+        query = {"context": context, "question": question, "chat_history": chat_history}
+
+        # Intentar obtener la respuesta de la cadena
+        result = chain.invoke(query)
+        print(query)
+        answer = result["answer"]
+        
+    except Exception as e:
+        # Manejar cualquier excepción que ocurra durante la recuperación
+        print(f"Error retrieving answer: {e}")
+        answer = "Ocurrió un error al procesar la solicitud, por favor, intentalo más tarde."
+
+    # Agregar el nuevo mensaje del usuario al historial del chat
+    chat_history.append(HumanMessage(content=question))
+    chat_history.append(AIMessage(content=answer))
+
+    print("Chat history: ", chat_history)
+    print("Answer final")
+    print(answer)
+
+    # Devolver la respuesta JSON
+    return JSONResponse(content={"answer": answer})
+
+# Con ChromaDB
+""" @app.post("/send")
 async def send_message(request: Request):
     global vector_store, chat_history
 
@@ -128,13 +237,14 @@ async def send_message(request: Request):
         vectordb = Chroma(persist_directory="./persist", embedding_function=embeddings)
 
         # Verificar el contenido del vector store
-        """ print("Contenido del Vector Store:")
+        Eliminar doble comentarios 
+        """ """ print("Contenido del Vector Store:")
         all_docs = vectordb.similarity_search("", k=5)
         for doc in all_docs:
-            print(doc.page_content) """
+            print(doc.page_content) """ """
 
         # Definir el template y las variables de entrada
-        template = """
+        template = """ """
         As a highly knowledgeable assistant specialized in aiding analyst programmers from Bantotal in comprehending client requirements, your task is to respond to questions in Spanish related to the documents delivered by the business analysis area. 
         These documents contain requirement information and a technical solution, including the logic that the program will execute, the database tables involved, and programs related to the solution's development.
 
@@ -146,7 +256,7 @@ async def send_message(request: Request):
         Question: {question}
 
         Helpful Answer:
-        """
+        """ """
 
         input_variables = ["context", "question"]
         QA_CHAIN_PROMPT = PromptTemplate(template=template, input_variables=input_variables)
@@ -196,7 +306,7 @@ async def send_message(request: Request):
     print(answer)
 
     # Devolver la respuesta JSON
-    return JSONResponse(content={"answer": answer})
+    return JSONResponse(content={"answer": answer}) """
 
 #----------------------------------------------------------------------------------------
 
