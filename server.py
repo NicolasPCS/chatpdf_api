872c6cac6ee9ae70e97a239d5bd0954d3e307748
Nicolás Caytuiro <ncaytuiro@bantotal.com>
2024-06-21 12:32:35 -0500

    Modificada la carga de archivos

--- server.py
+++ server.py
@@ -84,8 +84,18 @@ async def upload_file(user_id: str = Form(...), file: UploadFile = File(...)):
         print("Documentos cargados en el vector store:")
         """ for doc in docs:
             print(doc)  # Imprime los primeros 200 caracteres de cada documento """
+        
+        question = "Describe detalladamente todas las secciones del documento"
+        result = agent(question)
+        #print(query)
+        answer = result["answer"]
 
-        return {"message": f"El archivo '{file.filename}' ha sido cargado y procesado correctamente."}
+        query = {
+            "message": f"El archivo '{file.filename}' ha sido cargado y procesado correctamente.",
+            "description": answer
+            }
+
+        return JSONResponse(content=query)
     
     except Exception as e:
         # Capturar y mostrar el error detallado
@@ -109,65 +119,8 @@ async def send_message(request: ChatRequest):
         return JSONResponse(content={"message": f"No se ha cargado ningún archivo PDF para este usuario. Usuario Nro. {user_id}. Usuario Dir. {user_dir}"}, status_code=400)
 
     try:
-        # Definir el template y las variables de entrada
-        template = """
-         Task: You are a helpful assistant, an expert in the core of Bantotal.
-         Your task is to assist Bantotal's analyst programmers in understanding the development of a requirement in Genexus. 
-         You will provide relevant information based on the user's requests. The user will upload a document containing all the information about the requirement and will ask you questions about this file.
-
-         Instructions:
-
-         - You must answer the user's questions in SPANISH clearly, precisely, and in detail, ensuring that you use only the information provided in the document, the chat history, and any prior knowledge about Genexus and Bantotal that you possess.
-         - If a question cannot be answered based on the information in the document or your knowledge, you should ask the user to provide more information or context. Otherwise, honestly state that you cannot answer the question.
-         - Be detailed in your responses, but stay focused on the question. Add all useful details to provide a complete answer, but do not include details that are outside the scope of the question.
-         - Below is an example of the structure that a requirements document may have:
-           Example of the structure:
-             - Objective: Helps analyst programmers understand the objective of the requirement.
-             - Scope: Helps analyst programmers understand the scope of the requirement.
-             - Requirement Description: Provides a detailed understanding of the requirement.
-             - Proposed Solution:
-                 - Functional Solution/Functional Solution Description: Considerations before starting the development of the requirement.
-                 - Technical Solution/Technical Solution Description: Includes the solution in pseudocode or critical technical aspects for development in Genexus.
-             - Validation Matrix: Validates that the program meets the functionalities described in the objective of the requirement.
-           In most cases, this structure is followed. However, some documents may not strictly adhere to this structure. Make sure to answer the user's questions using only the information contained in the PDF, which is stored in a vector database.
-
-        Context: {context}
-
-        Question: {question}
-
-        Helpful Answer:
-        """
-
-        input_variables = ["context", "question"]
-        QA_CHAIN_PROMPT = PromptTemplate(template=template, input_variables=input_variables)
-
-        # Configuración del modelo LLM y embeddings
-        llm = AzureChatOpenAI(
-            deployment_name="gpt-35-turbo-16k", 
-            temperature=0
-        )
-
-        # Crear la cadena de conversación con recuperación utilizando un prompt personalizado
-        chain = ConversationalRetrievalChain.from_llm(
-            llm=llm,
-            retriever=vector_store.as_retriever(search_kwargs={"k": 5}),
-            return_source_documents=True,
-            combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT},
-        )
-
-        # Generar el contexto solo con mensajes de usuario y respuestas del asistente
-        context_list = []
-        documents = vector_store.similarity_search(query=question, k=5)
-        for doc in documents:
-            #print(doc)
-            context_list.append(doc.page_content)
-        context = " ".join(context_list)
-
-        # Pasar la cadena de texto como contexto en el diccionario query
-        query = {"context": context, "question": question, "chat_history": chat_history}
-
         # Intentar obtener la respuesta de la cadena
-        result = chain.invoke(query)
+        result = agent(question)
         #print(query)
         answer = result["answer"]
         
@@ -186,6 +139,66 @@ async def send_message(request: ChatRequest):
 
     # Devolver la respuesta JSON
     return JSONResponse(content={"answer": answer})
+
+def agent(question):
+    # Definir el template y las variables de entrada
+    template = """
+        Task: You are a helpful assistant, an expert in the core of Bantotal.
+        Your task is to assist Bantotal's analyst programmers in understanding the development of a requirement in Genexus. 
+        You will provide relevant information based on the user's requests. The user will upload a document containing all the information about the requirement and will ask you questions about this file.
+
+        Instructions:
+
+        - You must answer the user's questions in SPANISH clearly, precisely, and in detail, ensuring that you use only the information provided in the document, the chat history, and any prior knowledge about Genexus and Bantotal that you possess.
+        - If a question cannot be answered based on the information in the document or your knowledge, you should ask the user to provide more information or context. Otherwise, honestly state that you cannot answer the question.
+        - Be detailed in your responses, but stay focused on the question. Add all useful details to provide a complete answer, but do not include details that are outside the scope of the question.
+        - Below is an example of the structure that a requirements document may have:
+        Example of the structure:
+            - Objective: Helps analyst programmers understand the objective of the requirement.
+            - Scope: Helps analyst programmers understand the scope of the requirement.
+            - Requirement Description: Provides a detailed understanding of the requirement.
+            - Proposed Solution:
+                - Functional Solution/Functional Solution Description: Considerations before starting the development of the requirement.
+                - Technical Solution/Technical Solution Description: Includes the solution in pseudocode or critical technical aspects for development in Genexus.
+            - Validation Matrix: Validates that the program meets the functionalities described in the objective of the requirement.
+        In most cases, this structure is followed. However, some documents may not strictly adhere to this structure. Make sure to answer the user's questions using only the information contained in the PDF, which is stored in a vector database.
+
+    Context: {context}
+
+    Question: {question}
+
+    Helpful Answer:
+    """
+
+    input_variables = ["context", "question"]
+    QA_CHAIN_PROMPT = PromptTemplate(template=template, input_variables=input_variables)
+
+    # Configuración del modelo LLM y embeddings
+    llm = AzureChatOpenAI(
+        deployment_name="gpt-35-turbo-16k", 
+        temperature=0
+    )
+
+    # Crear la cadena de conversación con recuperación utilizando un prompt personalizado
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(search_kwargs={"k": 5}),
+        return_source_documents=True,
+        combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT},
+    )
+
+    # Generar el contexto solo con mensajes de usuario y respuestas del asistente
+    context_list = []
+    documents = vector_store.similarity_search(query=question, k=5)
+    for doc in documents:
+        #print(doc)
+        context_list.append(doc.page_content)
+    context = " ".join(context_list)
+
+    # Pasar la cadena de texto como contexto en el diccionario query
+    query = {"context": context, "question": question, "chat_history": chat_history}
+
+    return chain.invoke(query)
 
 #----------------------------------------------------------------------------------------
 
